85536f30e47a02db45b83a7a864cb98307c2056a
ocean-yumnam <yumnam.oceansana@inginit.com>
2025-05-22 22:56:00 +0530

    Add interval feature

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const name = "John Doe";
 setTimeout(() => {
   console.log(`Hello, ${name}! This is a test script.`);
 }, 1000);
+setInterval(() => {
+  console.log(`Hello, ${name}! This is a test script.`);
+}, 1000);
